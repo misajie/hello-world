a025790be9059e49cac44af50bb030fd6ab625a9
Tzay Lee <18914289890@163.com>
2019-09-30 20:12:11 +0800

    Add hello world script by Python

---
+++ hello-world.py
@@ -0,0 +1,11 @@
+# hello-world.py
+# Author: Misajie
+# Data: 2019/09/30
+class CHtool:
+    __hello = "hello-world"
+    def disp(self):
+        print(self.__hello)
+
+if __name__=='__main__':
+    ob1 = CHtool()
+    ob1.disp()
